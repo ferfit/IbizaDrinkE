d0df434eb3f31a1b04af8eaabca1137ef4879719
ferfit <ferfit16@gmail.com>
2021-05-01 22:13:51 -0300

    cree pagina inicio

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+const hamburguesa = document.querySelector('#hamburger');
+ const menu = document.querySelector('#menu');
+
+
+hamburguesa.addEventListener('click', ()=>{ 
+    hamburguesa.classList.toggle('animate');
+    menu.classList.toggle('menu--mostrar');
+})
+
+
