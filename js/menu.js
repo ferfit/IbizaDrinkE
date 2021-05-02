dd703b819add1a63da2bebfac1234fdffa338383
ferfit <ferfit16@gmail.com>
2021-05-02 00:31:52 -0300

    agregue configuración del menu de navegacion

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+// guardamos el elemento en una variable para luego trabajar  con ella
+var contenedor = document.querySelector('.header__contenedor-principal');
+var link = document.querySelectorAll('.menu__link');
+
+console.log(link);
+
+// creamos un evento que escuche el scroll y al detectarlo se ejecute una funcion,
+document.addEventListener('scroll',function(){   
+    var posicion = window.scrollY;
+    if(posicion>0){
+        contenedor.classList.add('menuBackground');
+        for (let i = 0; i < link.length; i++) {
+            link[i].classList.add('linkColor');  
+        }
+    } else{
+        contenedor.classList.remove('menuBackground');
+        for (let i = 0; i < link.length; i++) {
+            link[i].classList.remove('linkColor');  
+        }
+    }
+});
