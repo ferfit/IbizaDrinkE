341caba408373ce05a4049f2515ba45340914b38
ferfit <ferfit16@gmail.com>
2021-05-10 13:16:56 -0300

    varios cambios generales

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,10 @@
 // guardamos el elemento en una variable para luego trabajar  con ella
 var contenedor = document.querySelector('.header__contenedor-principal');
 var hamburger = document.querySelector('.hamburger');
+var hamburgerLinea = document.querySelectorAll('.hamburger__linea');
 var link = document.querySelectorAll('.menu__link');
+
+
 
 console.log(link);
 
@@ -11,13 +14,27 @@ document.addEventListener('scroll',function(){
     if(posicion>0){
         contenedor.classList.add('menuBackground');
         hamburger.classList.add('hamburgerTop');
+
+        for (let i = 0; i < hamburgerLinea.length; i++) {
+            hamburgerLinea[i].classList.add('hamburgerLineaColor');  
+        }
+
         for (let i = 0; i < link.length; i++) {
             link[i].classList.add('linkColor');  
         }
 
+        
+
+        
+
     } else{
         contenedor.classList.remove('menuBackground');
         hamburger.classList.remove('hamburgerTop');
+
+        for (let i = 0; i < hamburgerLinea.length; i++) {
+            hamburgerLinea[i].classList.remove('hamburgerLineaColor');  
+        }
+
         for (let i = 0; i < link.length; i++) {
             link[i].classList.remove('linkColor');  
         }
