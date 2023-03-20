8596b827f78727e0d0c9005f7d63f59b85e2e74c
fernandoinxap <fpereyra@hco.com>
2023-03-20 13:10:35 -0300

    traduccio ingles

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@
   });
   
     // Porfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
+    /* var portfolioIsotope = $('.portfolio-container').isotope({
       itemSelector: '.portfolio-item',
       layoutMode: 'fitRows'
     });
@@ -64,17 +64,37 @@
 
     setTimeout(function () {
         $('#rejas').click();
-    }, 1000);
-  
+    }, 1000); */
+
+    // Intro carousel
+    /* var introCarousel = $(".carousel");
+    var introCarouselIndicators = $(".carousel-indicators");
+    introCarousel.find(".carousel-inner").children(".carousel-item").each(function(index) {
+    (index === 0) ?
+    introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "' class='active'></li>") :
+    introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "'></li>");
+
+    $(this).css("background-image", "url('" + $(this).children('.carousel-background').children('img').attr('src') +"')");
+    $(this).children('.carousel-background').remove();
+    });
+
+    $(".carousel").swipe({
+    swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+        if (direction == 'left') $(this).carousel('next');
+        if (direction == 'right') $(this).carousel('prev');
+    },
+    allowPageScroll:"vertical"
+    });
+   */
     
-  })(jQuery);
+})(jQuery);
   
   
   // variable 
-var whatsapp = document.querySelector('.whatsapp');
+/* var whatsapp = document.querySelector('.whatsapp'); */
 
 // creamos un evento que escuche el scroll y al detectarlo se ejecute una funcion,
-document.addEventListener('scroll',function(){   
+/* document.addEventListener('scroll',function(){   
     var posicion = window.scrollY;
     if(posicion>1000){
         whatsapp.classList.add('mostrarWhatsapp');
@@ -82,4 +102,4 @@ document.addEventListener('scroll',function(){
         whatsapp.classList.remove('mostrarWhatsapp');
     }
 
-});
+}); */
