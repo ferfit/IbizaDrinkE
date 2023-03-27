cca04ee0046e01d39a8c69f4e13debfab79f3073
fernandoinxap <fpereyra@hco.com>
2023-03-27 10:23:39 -0300

    correcionesw

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 (function ($) {
     "use strict";
 
-    
-
-
-    console.log('holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    $(document).ready(function(){
+        var owl = $('.owl-carousel');
+        owl.owlCarousel({
+            items:1,
+            loop:true,
+            margin:0,
+            autoplay:true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:true
+        });
+        $('.play').on('click',function(){
+            owl.trigger('play.owl.autoplay',[1000])
+        })
+        $('.stop').on('click',function(){
+            owl.trigger('stop.owl.autoplay')
+        })
+    });
 
     // Testimonials carousel
     $(".testimonials-carousel").owlCarousel({
